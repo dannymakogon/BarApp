ce79cfea9f2afaaf3d12590c772c240426bbbb00
Dannymakogon <Dmakogon@uncc.edu>
2021-04-26 18:10:48 -0400

    Initial Commit

---
+++ Bar Order App/Bar_Order_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bar_Order_AppApp.swift
+//  Bar Order App
+//
+//  Created by Danny Makogon on 4/26/21.
+//
+
+import SwiftUI
+
+@main
+struct Bar_Order_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bar Order App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bar Order App
+//
+//  Created by Danny Makogon on 4/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
