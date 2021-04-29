867ed2b83f67b6425ea9806e8ab0c25978a4d487
Dannymakogon <dan.makogon@gmail.com>
2021-04-29 10:28:52 -0400

    added notifications... sik

--- Bar Order App/OrderScreen.swift
+++ Bar Order App/OrderScreen.swift
@@ -91,7 +91,26 @@ struct OrderScreen: View {
                   primaryButton: .default (Text("OK")) {
 //                    self.placeOrder()
                     //print(ViewChanger.$currentPage)
-                    orderConfirmed = true
+                    UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                        if success {
+                            print("All set!")
+                        } else if let error = error {
+                            print(error.localizedDescription)
+                        }
+                    }
+                    let content = UNMutableNotificationContent()
+                    content.title = "Your drink has been Ordered"
+                    content.subtitle = "We'll let you know when it's done"
+                    content.sound = UNNotificationSound.default
+
+                    // show this notification five seconds from now
+                    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+
+                    // choose a random identifier
+                    let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+
+                    // add our notification request
+                    UNUserNotificationCenter.current().add(request)
 
                     print("\(order.userName) ordered \(Order.drinks[order.type])")
                   }, secondaryButton: .cancel()
